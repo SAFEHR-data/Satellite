205ecbcd01ce37189d5d25dc89ce6f691b014798
tom <tom.young.316@gmail.com>
2022-12-20 14:23:57 +0000

    Start hostpial_visit

--- satellite/_fake.py
+++ satellite/_fake.py
@@ -99,25 +99,39 @@ class _StarPersonProvider(PersonProvider, _StarBaseProvider):
     def name(self) -> str:
         return self.text()
 
+    @staticmethod
+    def patient_class() -> None:
+        return None
 
-class _StarDatetimeProvider(FakerDTProvider):
+
+class _StarDatetimeProvider(FakerDTProvider, _StarBaseProvider):
     def timestamptz(self) -> datetime:
         return self.date_time()
+
+    def _recent_datetime(self) -> datetime:
+        delta_time = timedelta(
+            seconds=self.unix_time(start_datetime=datetime(2018, 1, 1),
+                                   end_datetime=datetime(2023, 1, 1))
+        )
+        return datetime(1970, 1, 1) + delta_time
 
     def valid_from(self) -> datetime:
         """
         Valid from is when EMAP star last updated this row. This is likely after ~2018
         """
-        return datetime(2018, 1, 1) + timedelta(seconds=self.unix_time())
+        return self._recent_datetime()
 
     def stored_from(self) -> datetime:
         """Valid from is when EMAP star first stored this row"""
-        return self.valid_from()
+        return self._recent_datetime()
 
     def date(self, **kwargs: Any) -> date:
         return self.date_between(
             date.fromisoformat("1970-01-01"), date.fromisoformat("2022-01-01")
         )
+
+    def discharge_datetime(self) -> datetime:
+        return self._value_or_none(self._recent_datetime(), p=0.1)
 
 
 class _StarAddressProvider(AddressProvider):
