7ab432780a605a532eb1901384dd6b56c5dd2721
tom <tom.young.316@gmail.com>
2023-03-09 10:32:47 +0000

    Allow clone with new token

--- satellite/main.py
+++ satellite/main.py
@@ -19,8 +19,11 @@ from satellite._tables import Tables
 from satellite._settings import EnvVar
 from satellite._utils import call_every_n_seconds
 
-REPO_URI = "github.com/UCLH-Foundry/Inform-DB"
 PAT = EnvVar("INFORMDB_PAT").unwrap()
+if PAT.startswith("github_pat"):  # is a fine-grained token
+    REPO_URL = f"https://oauth2:{PAT}@github.com/UCLH-Foundry/Inform-DB"
+else:
+    REPO_URL = f"https://{PAT}:x-oauth-basic@github.com/UCLH-Foundry/Inform-DB"
 
 star = DatabaseSchema(
     name=EnvVar("STAR_SCHEMA_NAME").or_default(),
@@ -29,7 +32,7 @@ star = DatabaseSchema(
     username=EnvVar("POSTGRES_USER").unwrap(),
     password=EnvVar("POSTGRES_PASSWORD").unwrap(),
     tables=Tables.from_repo(
-        repo_url=f"https://{PAT}:x-oauth-basic@{REPO_URI}",
+        repo_url=REPO_URL,
         branch_name=EnvVar("INFORMDB_BRANCH_NAME").or_default(),
     ),
 )
